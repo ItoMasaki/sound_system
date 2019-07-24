064989478c75764391a84c943add1cbc8be5e860
l1sum <l1sum@icloud.com>
2019-07-24 18:15:51 +0900

    update listen function

--- sound_system.py
+++ sound_system.py
@@ -48,7 +48,7 @@ class SoundSystem(Node):
                 self.speak(command[1].replace("Content:", ""))
 
             if "listen" == command[0].replace("Command:", ""):
-                self.listen()
+                self.listen(command[1].replace("Content:", ""))
            
 
     # detect hotword
@@ -87,14 +87,21 @@ class SoundSystem(Node):
         p.terminate()
 
 
-    # [TODO] please build this function!
-    def listen(self):
+    # listen any drammers
+    def listen(self, file_name):
+
+        # make dict and gram files path
+        path = "sound_system/dictionary/{0}".format(file_name)
+        dict_path = path + ".dict"
+        gram_path = path + ".gram"
+
+        # setup live_speech
+        if file_name == "":
+            self.setup_live_speech(False, None, None, 1e-20)
+        else:
+            self.setup_live_speech(False, dict_path, gram_path, 1e-20)
+        
         print("[*] LISTENING ...")
-        self.live_speech = LiveSpeech(lm=False,
-                                      hmm=os.path.join(self.model_path, 'en-us'),
-                                      dic=self.dic_path,
-                                      jsgf=self.gram_path,
-                                      kws_threshold=1e-20)
 
         for phrase in self.live_speech:
             print(phrase)
@@ -107,6 +114,7 @@ class SoundSystem(Node):
                                       dic=dict_path,
                                       jsgf=jsgf_path,
                                       kws_threshold=kws_threshold)
+
 
     # setup publisher for return to root topic
     def setup_topic(self, topic_name):
