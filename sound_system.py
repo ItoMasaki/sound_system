446beb05239c09eeae499d68a38e35430e25d3bb
yamatomatsuda <yamato19990708@gmail.com>
2019-07-25 16:55:06 +0900

    test済み pathがおかしい

--- sound_system.py
+++ sound_system.py
@@ -9,8 +9,27 @@ import rclpy
 from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data
 from std_msgs.msg import String,Float64
+
+
 from threading import Thread
 
+import usb
+import usb.core
+import usb.util
+import time
+import os
+import sys
+import struct
+
+dev = usb.core.find(idVendor=0x2886,idProduct=0x0018)
+
+PARAMETERS = {
+    'DOAANGLE': (21, 0, 'int', 359, 0, 'ro', 'DOA angle. Current value. Orientation depends on build configuration.'),
+    'SPEECHDETECTED': (19, 22, 'int', 1, 0, 'ro', 'Speech detection status.', '0 = false (no speech detected)',
+                       '1 = true (speech detected)')
+}
+
+TIMEOUT = 100000
 
 class SoundSystem(Node):
     def __init__(self):
@@ -19,15 +38,20 @@ class SoundSystem(Node):
         # ROS2
         self.create_subscription(String, "sound_system/command", self.command_callback, qos_profile_sensor_data)
         self.command = None
+
+        self.pub = self.create_publisher(Float64,"sound_system/direction",qos_profile_sensor_data)
         # Speak
         self.picotts = PicoTTS()
         # Pocketsphinx
-        self.hotword_dic_path = "sound_system/dictionary/hey_ducker.dict"
-        self.hotword_gram_path = "sound_system/dictionary/hey_ducker.gram"
+        self.hotword_dic_path = "/home/matsudayamato/python_ws/src/sound_system/dictionary/hey_ducker.dict"
+        self.hotword_gram_path = "/home/matsudayamato/python_ws/src/sound_system/dictionary/hey_ducker.gram"
         self.model_path = get_model_path()
         # respeaker
         self.dev = usb.core.find(idVendor=0x2886, idProduct=0x0018)
-        self.conter = 0
+        self.counter = 0
+        ###debug###
+        self.listen('hey_ducker')
+
 
 
     # recieve a command {Command, Content}
@@ -94,12 +118,10 @@ class SoundSystem(Node):
     def listen(self, file_name):
 
         # make dict and gram files path
-        path = "sound_system/dictionary/{0}".format(file_name)
-        dict_path = path + ".dict"
-        gram_path = path + ".gram"
-
+        dict_path = self.hotword_dic_path
+        gram_path = self.hotword_gram_path
         # setup live_speech
-        self.setup_live_speech(False, dict_path, gram_path, 1e-20)
+        self.setup_live_speech(False, dict_path, gram_path, 1e-10)
 
         print("[*] LISTENING ...")
 
@@ -109,13 +131,13 @@ class SoundSystem(Node):
     # get array from respeaker
     def get_array(self):
         while True:
-            if read('SPEECHDETECTED') == 1:
-                for kw in self.speech:
-                    direction = self.direction()
-                    if kw ! = " ":
+            if self.read('SPEECHDETECTED') == 1:
+                for kw in self.live_speech:
+                    angular = self.direction()
+                    if kw != " ":
                         self.counter += 1
-                        print(self.counter + ":" + direction, flush=True)
-                        self.pub.publish(temp)
+                        print(str(self.counter) + ":" + str(angular), flush=True)
+                        #self.pub.publish(float(angular))
 
 
     # read buffer data from respeaker
@@ -135,27 +157,25 @@ class SoundSystem(Node):
 
         length = 8
 
-        try:
-            response = dev.ctrl_transfer(
-                usb.util.CTRL_IN | usb.util.CTRL_TYPE_VENDOR | usb.util.CTRL_RECIPIENT_DEVICE,
-                0, cmd, id, length, TIMEOUT)
 
-            response = struct.unpack(b'ii', response.tostring())
+        response = dev.ctrl_transfer(
+            usb.util.CTRL_IN | usb.util.CTRL_TYPE_VENDOR | usb.util.CTRL_RECIPIENT_DEVICE,
+            0, cmd, id, length, TIMEOUT)
 
-            if data[2] == 'int':
-                result = response[0]
-            else:
-                result = response[0] * (2. ** response[1])
-	    except:
-	        result = 0
+        response = struct.unpack(b'ii', response.tostring())
+
+        if data[2] == 'int':
+            result = response[0]
+        else:
+            result = response[0] * (2. ** response[1])
+
 
         return result
 
 
     # get direction
-    @staticmethod
-    def direction():
-        read('DOAANGLE')
+    def direction(self):
+        return self.read('DOAANGLE')
 
 
     # setup livespeech
