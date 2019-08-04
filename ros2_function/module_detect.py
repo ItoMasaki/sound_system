ad09defff9dca6977dbfbba72d8431fc8b92aa5d
hir-osechi <rr0111fx@ed.ritsumei.ac.jp>
2019-08-04 17:03:49 +0900

    module_detect.detect()

--- ros2_function/module_detect.py
+++ ros2_function/module_detect.py
@@ -5,11 +5,14 @@ from pyaudio import PyAudio
 from picotts import PicoTTS
 from pocketsphinx import LiveSpeech, get_model_path
 
+import module_speak
+
 hotword_dic_path = "../dictionary/hey_ducker.dict"
 hotword_gram_path = "../dictionary/hey_ducker.gram"
 model_path = get_model_path()
 picotts = PicoTTS()
-        
+  
+#detect hotword      
 def detect():
     print("[*] START HOTWORD RECOGNITION")
     setup_live_speech(False, hotword_dic_path, hotword_gram_path, 1e-20)
@@ -18,29 +21,10 @@ def detect():
     for phrase in live_speech:
         print(phrase)
         if "hey ducker" == str(phrase):
-            speak("yes sir !")
+            module_speak.speak("yes sir !")
             live_speech.stop = True
             del(live_speech)
             break
-                    
-def speak(content):
-    print("[*] SPEAK : {0}".format(content))
-    p = PyAudio()
-    waves = picotts.synth_wav(content)
-    wav = wave.open(BytesIO(waves))
-    stream = p.open(format=p.get_format_from_width(wav.getsampwidth()),\
-                              channels=wav.getnchannels(),\
-                              rate=16000,\
-                              output=True)
-
-    data = wav.readframes(1024)
-    while data != b'':
-        stream.write(data)
-        data = wav.readframes(1024)
-
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
             
 # setup livespeech
 def setup_live_speech(lm, dict_path, jsgf_path, kws_threshold):
