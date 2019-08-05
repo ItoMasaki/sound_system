d0af7e63a8d3b188eee191e6505795a20f9bffc6
yamatomatsuda <yamato19990708@gmail.com>
2019-08-06 02:46:59 +0900

    リファクタ

--- setup.py
+++ setup.py
@@ -8,11 +8,10 @@ setup(
     packages=[],
     py_modules=[
         "sound_system",
-        "sound_detection"
     ],
     install_requires=['setuptools','pyusb','click'],
     zip_safe=True,
-    author='ItoMasaki,MatudaYamato',
+    author='ItoMasaki,MatudaYamato,HiroseChihiro',
     author_email='is0449sh@ed.ritsumei.ac.jp',
     maintainer='ItoMasaki,MatsudaYamto',
     maintainer_email='is0449sh@ed.ritsumei.ac.jp,is0476hv@ed.ritsumei.ac.jp',
@@ -23,13 +22,12 @@ setup(
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
-    description='Examples of minimal publishers using rclpy.',
+    description='sound package for SPR',
     license='Apache License, Version 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'sound_system = sound_system:main',
-            'sound_detection = sound_detection:main'
         ],
     },
 )
